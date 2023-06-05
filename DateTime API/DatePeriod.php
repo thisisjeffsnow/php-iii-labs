6e2c4ace03b9ab5a7b6a10ad6aa8559e8374d1ce
Jeff Snow <thisisjeffsnow@gmail.com>
2023-06-05 12:33:26 -0400

    Updates DatePeriod example.
    Updates DatePeriod example.
    
    Updates DatePeriod example

--- DateTime API/DatePeriod.php
+++ DateTime API/DatePeriod.php
@@ -1,8 +1,39 @@
 <?php
 
+# Works.
 $schedule = function () {
     $now = new DateTime('now');
     $int = DateInterval::createFromDateString('second friday of next month');
+    $now->add($int);
+    $period = new DatePeriod($now, $int, 12);
+    foreach ($period as $date)
+        yield $date->format('l, d M Y');
+};
+foreach ($schedule() as $date) echo $date . PHP_EOL;
+
+echo PHP_EOL;
+
+# Alternative, but misses last interval compared to previous.
+$schedule = function () {
+    $now = new DateTime('now');
+    // Set the date to the first day of the next month
+    $now->modify('first day of next month');
+    // Create a schedule for the second Friday of every month for a year
+    for ($i = 0; $i < 12; $i++) {
+        // Set the date to the second Friday of the month
+        $now->modify('second friday of this month');
+        yield $now->format('l, d M Y');
+        // Set the date to the first day of the next month
+        $now->modify('first day of next month');
+    }
+};
+foreach ($schedule() as $date) echo $date . PHP_EOL;
+
+
+# Doesn't work.
+$schedule = function () {
+    $now = new DateTime('now');
+    $int = DateInterval::createFromDateString('second friday of every month');
     $now->add($int);
     $period = new DatePeriod($now, $int, 12);
     foreach ($period as $date)
